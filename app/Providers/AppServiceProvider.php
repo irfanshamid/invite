d8e820311dcf4f124c80021e9bc63014f1d2fb44
Irfan Hamid <irfansyukri597@gmail.com>
2023-03-27 00:45:44 +0700

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use \Illuminate\Support\Facades\URL;
+if ($this->app->environment('production')) {
+    URL::forceScheme('https');
+}
 
 class AppServiceProvider extends ServiceProvider
 {
